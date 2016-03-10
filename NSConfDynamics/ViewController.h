a63efb9b7cfb350151f3932dda9820fc96680c78
Rodrigo N Mato <rodrigo.n.mato@gmail.com>
2016-03-10 20:57:29 -0300

    First commit - new project

---
+++ NSConfDynamics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSConfDynamics
+//
+//  Created by Rodrigo Mato on 3/10/16.
+//  Copyright © 2016 RodrigoMato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSConfDynamics/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSConfDynamics
+//
+//  Created by Rodrigo Mato on 3/10/16.
+//  Copyright © 2016 RodrigoMato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
